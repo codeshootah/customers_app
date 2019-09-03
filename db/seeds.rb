011388ce5ef613b207cbcfbe442eaa87d2d0c2eb
codeshootah <grigorycode@gmail.com>
2019-09-03 11:48:02 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Customer.create(name: 'Lorem', phone: '+1 234 567 89 00', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
+
+Customer.create(name: 'Schlitz', phone: '+1 234 567 89 00', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
+
+Customer.create(name: 'Celiac', phone: '+1 234 567 89 00', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
+
+Customer.create(name: 'Beard', phone: '+1 234 567 89 00', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
+
+Customer.create(name: 'Chambray', phone: '+1 234 567 89 00', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
+
+Customer.create(name: 'Pork', phone: '+1 234 567 89 00', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
