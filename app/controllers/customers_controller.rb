59595448857737ec65b3db805a053154666924f2
codeshootah <grigorycode@gmail.com>
2019-09-03 12:53:17 +0300

    Push customer to black list

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,6 @@
 class CustomersController < ApplicationController
+  before_action :find_customer, only: [:destroy]
+
   def index
     @customers = Customer.all
   end
@@ -6,4 +8,16 @@ class CustomersController < ApplicationController
   def black_list
     @customers = Customer.all
   end
+
+  def destroy
+    @customer.update(black_list: true)
+    redirect_to root_path
+  end
+
+  private
+ 
+  def find_customer
+    @customer = Customer.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'customers#index'
 
   get '/customers/black_list', to: 'customers#black_list', as: 'black_list'
+
+  resources :customers, only: [:destroy]
 end
