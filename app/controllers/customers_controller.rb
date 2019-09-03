a965e366deacf9969631b4b910ed53210dfa7b22
codeshootah <grigorycode@gmail.com>
2019-09-03 12:00:55 +0300

    Add black_list action

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,7 @@ class CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
+
+  def black_list
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'customers#index'
+
+  get '/customers/black_list', to: 'customers#black_list', as: 'black_list'
 end
