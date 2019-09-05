102cd3d0b750f08140f0ea6cc27b6ae8e48c66be
codeshootah <grigorycode@gmail.com>
2019-09-05 13:28:11 +0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Customer.create(name: 'Lorem', phone: '+1 234 567 89 00', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
+Customer.create(name: 'Lorem', phone: '+7 111 111 11 11', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
 
-Customer.create(name: 'Schlitz', phone: '+1 234 567 89 00', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
+Customer.create(name: 'Schlitz', phone: '+7 222 222 22 22', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
 
-Customer.create(name: 'Celiac', phone: '+1 234 567 89 00', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
+Customer.create(name: 'Celiac', phone: '+7 333 333 33 33', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
 
-Customer.create(name: 'Beard', phone: '+1 234 567 89 00', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: true)
+Customer.create(name: 'Beard', phone: '+7 444 444 44 44', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: true)
 
-Customer.create(name: 'Chambray', phone: '+1 234 567 89 00', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: true)
+Customer.create(name: 'Chambray', phone: '+7 555 555 55 55', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: true)
 
-Customer.create(name: 'Pork', phone: '+1 234 567 89 00', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
+Customer.create(name: 'Pork', phone: '+7 666 666 66 66', description: 'Tilde drinking vinegar selvage enamel pin, wolf tbh man bun vape.', black_list: false)
