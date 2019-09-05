9704f13737a9a37815fc33b0884409d2540c59a1
codeshootah <grigorycode@gmail.com>
2019-09-05 08:12:36 +0300

    Correct route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'customers#index'
 
-  get '/customers/black_list', to: 'customers#black_list', as: 'black_list'
+  get '/customers/black_list', to: 'customers#black_list', as: :black_list
   put 'enable/:id', to: 'customers#enable', as: :enable_customer
 
   resources :customers
